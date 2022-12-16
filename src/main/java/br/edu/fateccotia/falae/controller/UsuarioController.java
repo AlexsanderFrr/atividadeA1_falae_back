76be39c02dd60fab12ec6c9437eca7790de921a8
TheuTheuutra <105327246+TheuTheuutra@users.noreply.github.com>
2022-12-15 22:37:47 -0300

    Quinto Commit

--- src/main/java/br/edu/fateccotia/falae/controller/UsuarioController.java
+++ src/main/java/br/edu/fateccotia/falae/controller/UsuarioController.java
@@ -3,7 +3,9 @@ package br.edu.fateccotia.falae.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,55 +13,75 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.fateccotia.falae.model.User;
 import br.edu.fateccotia.falae.service.UsersService;
 
-
 @RestController
 @RequestMapping("/users")
-
 public class UsuarioController {
 	@Autowired
 	private UsersService usersService;
+	private final PasswordEncoder encoder;
 	
+	public UsuarioController(PasswordEncoder encoder) {
+		this.encoder = encoder;
+        
+    }
+
+
 	@PostMapping
-	public ResponseEntity<User> create(@RequestBody User user){
+	public ResponseEntity<User> create(@RequestBody User user) {
 		User created = usersService.save(user);
 		return ResponseEntity.ok(created);
-		
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<User> find(@PathVariable(name ="id") Integer id){
+	public ResponseEntity<User> find(@PathVariable(name = "id") Integer id) {
 		Optional<User> user = this.usersService.findById(id);
-		if(user.isPresent()) {
+		if (user.isPresent()) {
 			return ResponseEntity.ok(user.get());
-		}else {
+		} else {
 			return ResponseEntity.ok(user.get());
 		}
 	}
 	
-	
+	@GetMapping("/validarSenha")	
+	public ResponseEntity<Boolean> validarSenha(@RequestParam String email,
+												@RequestParam String password){
+
+		Optional<User> optuser = usersService.findByEmail(email);
+//		if (optuser.isEmpty()) {
+//			return ResponseEntity<Boolean>()
+//		}
+		
+		boolean valid = encoder.matches(password, optuser.get().getPassword());
+		
+		HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+		return ResponseEntity.status(status).body(valid);
+		
+	}
+
 //	public ResponseEntity<Users> get(@PathVariable(name = "id") Integer id){
 //		Users created = user.findById(id).get();
 //		
 //		if(userService.\findById(id).isPresent()) {
 //			created = u
-@PutMapping
-	public ResponseEntity<User> recuperate(@RequestBody User user){
+	@PutMapping
+	public ResponseEntity<User> recuperate(@RequestBody User user) {
 		User created = usersService.save(user);
 		return ResponseEntity.ok(created);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<User> delete(@PathVariable("id") Integer id){
-		
-		if(usersService.findById(id).isPresent()) {
+	public ResponseEntity<User> delete(@PathVariable("id") Integer id) {
+
+		if (usersService.findById(id).isPresent()) {
 			usersService.delete(id);
 		}
 		return ResponseEntity.ok(null);
 	}
-	
+
 }
--- src/main/java/br/edu/fateccotia/falae/service/UsersService.java
+++ src/main/java/br/edu/fateccotia/falae/service/UsersService.java
@@ -2,7 +2,7 @@ package br.edu.fateccotia.falae.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.edu.fateccotia.falae.model.User;
@@ -11,12 +11,16 @@ import br.edu.fateccotia.falae.repository.UsersRepository;
 @Service
 public class UsersService {
 	private final UsersRepository userRepository;
+	private final PasswordEncoder encoder;
 	
-	public UsersService(UsersRepository userRepository) {
+	
+	public UsersService(UsersRepository userRepository, PasswordEncoder encoder) {
+		this.encoder = encoder;
         this.userRepository = userRepository;
     }
 	
 	public User save(User user) {
+		user.setPassword(encoder.encode(user.getPassword()));
 		return this.userRepository.save(user);
 	}
 	
